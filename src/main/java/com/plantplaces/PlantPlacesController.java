686d29a121e4d61e72f846b0e9710e97f7c6e6dd
Administrator <Administrator@DESKTOP-099UQRG>
2019-10-13 21:54:13 -0400

    Autowired

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,7 +1,13 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.plantplaces.dto.SpecimenDTO;
+import com.plantplaces.service.ISpecimenService;
 
 /**
  * @author Administrator
@@ -11,14 +17,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
+	
+	@Autowired
+	private ISpecimenService specimenServiceStub;
 
 	
-	@RequestMapping("/start")
-	public String start() {
+	@RequestMapping(value="/start", method=RequestMethod.GET)
+	public String read() {
+	SpecimenDTO specimenDTO = specimenServiceStub.fetchById(43);
+		return "start";
+	}
+	@RequestMapping(value="/start", method=RequestMethod.GET, headers= {"content-type=text/json"})
+	public String readJSON() {
 		
 		return "start";
 	}
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=blue"})
+	public String readBlue() {
+		
+		return "start";
+	}
+
+	@RequestMapping(value="/start", method=RequestMethod.GET, params= {"loyalty=silver"})
+	public String readSilver() {
+		
+		return "start";
+	}
+
 	
+	@PostMapping("/start")
+public String create() {
+		
+		return "start";
+	}
 	/**
 	 * Handle the / endpoint
 	 *@return
